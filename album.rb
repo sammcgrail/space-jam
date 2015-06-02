0d88cf6eab99f638d945db75f951d6aac5b96199
sammcgrail <sammcgrail@gmail.com>
2015-06-02 11:36:39 -0400

    startginb build tracklist hash

--- album.rb
+++ album.rb
@@ -9,11 +9,42 @@ class Album
     @tracks = []
   end
 
+  def build_tracklist_hash
+    tracklist_hash = {}
+    @tracks.each do |track_info|
+      if track_info[:album_id] == id
+        tracklist_hash[track_info[:track_number]] = track_info[:title]
+      end
+    end
+  end
+
+  def sort_tracklist_hash
+
+  end
+
+
+  # return string of tracknames
+  # iterate through array of hashes
+  # each hash contain one track data
+
+  # if album id =
+  # add track name to tracklist
+
+
   def summary
     <<-SUMMARY
-    Track id: #{id}
-    Title: #{title}
-    Artist: #{artist}\n
+    Album id: #{id}
+    Album Title: #{title}
+    Album Artist: #{artist}\n
 SUMMARY
   end
+
+
+
+
+
+
+
+
+
 end
